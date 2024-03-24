f04123d549f3c319bb1eaa31c0844e9b14da8424
UBC Student <georgeso@students.cs.ubc.ca>
2024-03-24 10:59:39 -0700

    message

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -7,46 +7,55 @@ import ProfileCard from "../components/ProfileCard";
 import Filter from '../components/Filter'
 
 
-const List = () => {
-   // Sample profile data
-   const profiles = [
-       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
-       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano. I also enjoy talking which serves only to increase the wordcount so I can see what happens if there is a block of text' },
-       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
-       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
-       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
-       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
-       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
-       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
-       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
-       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
-       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer',  location: 'Vancouver', about: 'I love music!' },
-       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer',  location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
-       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer',  location: 'Vancouver', about: 'I love music!' },
-       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer',  location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
-   ];
 
-   return (
-       <>
-           <div className="profile-list">
-               {profiles.map((profile, index) => (
-                   <ProfileCard
-                       key={index}
-                       name={profile.name}
-                       email={profile.email}
-                       instrument={profile.instrument}
-                       level={profile.level}
-                       location={profile.location}
-                       about={profile.about}
-                   />
-               ))}
-           </div>
-           <div className="nav-bar-fixed-bottom">
-               <LabelBottomNavigation />
-           </div>
-       </>
-   );
+
+const List = () => {
+  // Sample profile data
+  const profiles = [
+      { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
+      { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano. I also enjoy talking which serves only to increase the wordcount so I can see what happens if there is a block of text' },
+      { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
+      { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+      { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
+      { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+      { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
+      { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+      { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer', location: 'Vancouver', about: 'I love music!' },
+      { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+      { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer',  location: 'Vancouver', about: 'I love music!' },
+      { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer',  location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+      { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', level: 'begineer',  location: 'Vancouver', about: 'I love music!' },
+      { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', level: 'begineer',  location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+  ];
+
+
+  return (
+      <>
+          <div className="filter-component">
+              <Filter />
+          </div>
+          <div className="profile-list">
+              {profiles.map((profile, index) => (
+                  <ProfileCard
+                      key={index}
+                      name={profile.name}
+                      email={profile.email}
+                      instrument={profile.instrument}
+                      level={profile.level}
+                      location={profile.location}
+                      about={profile.about}
+                  />
+              ))}
+          </div>
+          <div className="nav-bar-fixed-bottom">
+              <LabelBottomNavigation />
+          </div>
+      </>
+  );
 };
 
 
+
+
 export default List;
+
