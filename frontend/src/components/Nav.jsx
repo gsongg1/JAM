306fb7f438f6748df0ae346b25cb93f080a21b65
caudeximation <jungwoo.k.kim@gmail.com>
2024-03-23 23:36:37 -0700

    added text name in craete profile

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -6,27 +6,31 @@ import MenuIcon from '@mui/icons-material/Menu';
 import StarsIcon from '@mui/icons-material/Stars';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
+
 export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState('recents');
-  const navigate = useNavigate();
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-    if (newValue === 'profile') {
-      navigate('/profile'); //navigate to profile page
-    } else if (newValue === 'list') {
-      navigate('/list'); // Navigate to the List page
-    }
-  };
+ const [value, setValue] = React.useState('recents');
+ const navigate = useNavigate();
 
 
-  return (
-    <div  className="nav-container"> 
-    <BottomNavigation value={value} onChange={handleChange}>
-      <BottomNavigationAction label="Match" value="match" icon={<StarsIcon />} />
-      <BottomNavigationAction label="List" value="list" icon={<MenuIcon />} />
-      <BottomNavigationAction label="Profile" value="profile" icon={<AccountCircleIcon />} />
-    </BottomNavigation>
-    </div>
-  );
+ const handleChange = (event, newValue) => {
+   setValue(newValue);
+   if (newValue === 'profile') {
+     navigate('/profile'); //navigate to profile page
+   } else if (newValue === 'list') {
+     navigate('/list'); // Navigate to the List page
+   }
+ };
+
+
+
+
+ return (
+   <div  className="nav-container">
+   <BottomNavigation value={value} onChange={handleChange}>
+     <BottomNavigationAction label="Match" value="match" icon={<StarsIcon />} />
+     <BottomNavigationAction label="List" value="list" icon={<MenuIcon />} />
+     <BottomNavigationAction label="Profile" value="profile" icon={<AccountCircleIcon />} />
+   </BottomNavigation>
+   </div>
+ );
 }
--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -5,30 +5,47 @@ import LabelBottomNavigation from '../components/Nav';
 import '../App.css';
 import ProfileCard from "../components/ProfileCard";
 
-const List = () => {
-    // Sample profile data
-    const profiles = [
-        { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', location: 'Vancouver', about: 'I love music!' },
-        { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
-    ];
 
-    return (
-        <div className="profile-list">
-            {profiles.map((profile, index) => (
-                <ProfileCard
-                    key={index}
-                    name={profile.name}
-                    email={profile.email}
-                    instrument={profile.instrument}
-                    location={profile.location}
-                    about={profile.about}
-                />
-            ))}
-    
-                <LabelBottomNavigation />
-            </div>
-        
-    );
+const List = () => {
+   // Sample profile data
+   const profiles = [
+       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', location: 'Vancouver', about: 'I love music!' },
+       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', location: 'Vancouver', about: 'I love music!' },
+       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', location: 'Vancouver', about: 'I love music!' },
+       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', location: 'Vancouver', about: 'I love music!' },
+       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', location: 'Vancouver', about: 'I love music!' },
+       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', location: 'Vancouver', about: 'I love music!' },
+       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+       { name: 'John Doe', email: 'john@example.com', instrument: 'Guitar', location: 'Vancouver', about: 'I love music!' },
+       { name: 'Jane Smith', email: 'jane@example.com', instrument: 'Piano', location: 'Central Vancouver', about: 'I am passionate about playing the piano.' },
+   ];
+
+
+   return (
+       <>
+           <div className="profile-list">
+               {profiles.map((profile, index) => (
+                   <ProfileCard
+                       key={index}
+                       name={profile.name}
+                       email={profile.email}
+                       instrument={profile.instrument}
+                       location={profile.location}
+                       about={profile.about}
+                   />
+               ))}
+           </div>
+           <div className="nav-bar-fixed-bottom">
+               <LabelBottomNavigation />
+           </div>
+       </>
+   );
 };
+
 
 export default List;
